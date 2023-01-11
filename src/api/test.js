305eaf5e741dab49774316cbe79f4ba721eea661
Daniel Balloch <danielpbballoch@gmail.com>
2023-01-12 00:54:26 +1300

    basic form w/ sidebar working

--- src/api/test.js
+++ src/api/test.js
@@ -2,12 +2,28 @@ import fetch from "node-fetch"
 
 
 export default async (req, res) => {
+    console.log(req.body);
+    let TotalIncome;
+    let TotalLiabilities;
+    let Deposit;
+    if (req.body){
+        TotalIncome = parseInt(req.body.total_income);
+        TotalLiabilities = parseInt(req.body.total_liabilities);
+        Deposit = parseInt(req.body.deposit);
+    }
+    
     const url = `https://test-api-self.vercel.app/calculate`;  
     let api_body = JSON.stringify({
+        total_income: TotalIncome,
+        total_liabilities: TotalLiabilities,
+        deposit: Deposit,
+    })
+    let api_body2 = JSON.stringify({
         total_income: 150000,
         total_liabilities: 20000,
-        deposit: 100000,
+        deposit: 10000,
     })
+
     const options = {
     method: 'POST', 
         headers: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,36 @@ import InputComponent from "../components/input-component";
 const Wrapper = styled.div`
 display: flex;
 justify-content: center;
-flex-direction: column;
+flex-direction: row;
 max-width: 900px;
 width: 100%;
 margin: auto;
+form {
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    
+    input,button {
+        width: 100%;
+        margin: 10px;
+    }
+}
+div {
+    width: 50%;
+    margin: 40px;
+    padding: 20px;
+}
+.sidebar {
+    background-color: lightGray;
+    border-radius: 5px;
+    width: 230px;
+    p {
+        font-size: 17px;
+    }
+    .subp {
+        font-size: 15px;
+    }
+}
 `
 const ButtonDiv = styled.div`
 display: flex;
@@ -23,9 +49,13 @@ button {
 
 
 export default function Index(){
-    const [serverState, setServerState] = useState({
-        formSent: false,
-    });
+    const [serverState, setServerState] = useState({formSent: false});
+    const [totalIncome, setTotalIncome] = useState(0);
+    const [deposit, setDeposit] = useState(0);
+    const [canBorrow, setCanBorrow] = useState(0);
+    const [propertyValue, setPropertyValue] = useState(0);
+
+    
 
     const {
         register,
@@ -34,6 +64,8 @@ export default function Index(){
       } = useForm()
 
     async function onSubmit(data){
+        setDeposit(data.Deposit)
+        setTotalIncome(data.TotalIncome)
         fetch(`/api/test`, {
           method: `POST`,
           body: JSON.stringify({
@@ -48,6 +80,9 @@ export default function Index(){
           .then(res => res.json())
           .then(body => {
             console.log(`response from API:`, body);
+            
+            setCanBorrow(body.result.borrowing);
+            setPropertyValue(body.result.property);
           })
           .then(setServerState({formSent: true}))
       }
@@ -68,7 +103,7 @@ export default function Index(){
                 id="main-form"
                 >
                     <label htmlFor="total_income">
-                            <p>Deposit:</p>
+                            <p>Income:</p>
                             <input 
                                 type="text" 
                                 name="total_income" 
@@ -102,13 +137,18 @@ export default function Index(){
                     ><b>Send</b></button>
                     
                 </form>
-            <form>
+                <div className="sidebar">
+                    <p>Here's what you can borrow: <b>${canBorrow}</b></p>
+                    <p className="subp">With your deposit of ${deposit} you could afford a property upto ${propertyValue}</p>
+                    <p>Total Income <br/>${totalIncome}</p>
+                </div>
+            {/* <form>
                 <InputComponent 
                 Q1="How many of you are buying the property?" 
                 Q1O1="Just me" Q1O2="I'm buying with someone else" 
                 Q2="What's your base salary/wages?(before tax)?"
                 Q3="What's the second applicants base salary/wages?"/>
-            </form> 
+            </form>  */}
         </Wrapper>
     )
 }
